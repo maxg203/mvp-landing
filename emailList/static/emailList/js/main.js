180597678bb5f372dc70531dcb8c9e61931499c1
Max Goodridge <max.goodridge@hotmail.co.uk>
2016-07-20 21:50:58 +0100

    Decided against the home screen animation

--- emailList/static/emailList/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('#vision').delay(2000).hide(500)
-    window.setTimeout(function() {
-        document.getElementById('vision').innerHTML = "The Vision"
-    }, 2500)
-    $('#vision').delay(2500).show(500)
-})
