310aeac400cb60fdf152c9fef6999fad64d26dd8
Max Goodridge <max.goodridge@hotmail.co.uk>
2016-07-28 10:31:20 -0700

    Landing page source code done

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,17 +77,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 """
 Database Configuration for Production Server
-"""
- DATABASES = {
+
+DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.mysql',
          'NAME': 'landingPageDB',
@@ -96,8 +96,8 @@ Database Configuration for Production Server
          'HOST': 'localhost',
          'PORT': '',
      }
- }
-
+}
+"""
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
